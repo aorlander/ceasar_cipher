01aa33270a9353989c18f1349c6076fe46eea3b7
aorlander <github.com/aorlander/cit582.git>
2021-01-17 17:33:35 -0500

    com

--- ceasar.py
+++ ceasar.py
@@ -1,11 +1,11 @@
 def encrypt(key,plaintext):
-    ciphertext = ''
+    ciphertext = ""
     for c in plaintext:
         ciphertext = ciphertext + chr((ord(c) + key - 65) % 26 + 65)
-    print(ciphertext)
     return ciphertext
 
 def decrypt(key,ciphertext):
     plaintext = ""
-    
+    for c in ciphertext:
+        plaintext = plaintext + chr((ord(c) - key - 65) % 26 + 65)
     return plaintext
